ea88a8988e86ca4aacdc66438f377ed330a16e24
BTSQL <jungho8379@gmail.com>
2018-01-08 22:56:44 +0900

    login done

--- sqlccpjt/wingstone/settings.py
+++ sqlccpjt/wingstone/settings.py
@@ -140,6 +140,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #LOGIN_URL = '/account/login/'
 #LOGOUT_URL = '/account/logout/'
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL ='/'
 
 #STATIC_URL = '/static/'
 #STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
--- sqlccpjt/wingstone/views.py
+++ sqlccpjt/wingstone/views.py
@@ -4,6 +4,7 @@ from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 class SqlcHomeView(TemplateView) :
     template_name = 'sqlchome.html'
@@ -17,3 +18,11 @@ class SqlcUserCreateView(CreateView):
 class SqlcUserCreateDoneTV(TemplateView):
     template_name = 'sqlchome.html'
 
+"""
+#--- @login_required
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
+        return login_required(view)
+"""
